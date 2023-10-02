2739b97c55023b78510ff519324417741444c46c
Matthew Scott <matthewadamscott@googlemail.com>
2023-10-02 15:59:45 +0100

    Completed task 3

--- db/controllers.js
+++ db/controllers.js
@@ -1,7 +1,14 @@
-const { fetchTopics } = require("./models");
+const { fetchTopics, fetchAPI } = require("./models");
 
 exports.getTopics = (req, res) => {
   return fetchTopics().then((returnedTopics) => {
     return res.status(200).send({ topics: returnedTopics });
+  });
+};
+
+exports.getAPI = (req, res) => {
+  return fetchAPI().then((returnedEndpoints) => {
+    // console.log(returnedEndpoints);
+    return res.status(200).send({ endpoints: returnedEndpoints });
   });
 };
--- db/models.js
+++ db/models.js
@@ -1,5 +1,14 @@
 const db = require("../db/connection");
+const endpoints = require("../endpoints.json");
 
 exports.fetchTopics = () => {
-  return db.query("SELECT * FROM topics;");
+  return db.query("SELECT * FROM topics;").then((returnedTopics) => {
+    return returnedTopics.rows;
+  });
+};
+
+exports.fetchAPI = () => {
+  return new Promise((res, rej) => {
+    res(endpoints);
+  });
 };
