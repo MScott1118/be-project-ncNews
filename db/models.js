00d8ff9f9a31fa19e4a6da7da8969f132d1121ea
Matthew Scott <matthewadamscott@googlemail.com>
2023-10-03 15:34:35 +0100

    completed challenge 4

--- db/models.js
+++ db/models.js
@@ -12,3 +12,11 @@ exports.fetchAPI = () => {
     res(endpoints);
   });
 };
+
+exports.fetchArticleByID = (article_id) => {
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
+    .then((returnedArticle) => {
+      return returnedArticle.rows;
+    });
+};
